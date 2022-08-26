d562158b5bad9c1961b044ef4af8483d013ac4c5
Idriss Neumann <idriss.neumann@comwork.io>
2022-08-26 13:48:15 +0100

    Add dockerfile and entrypoint

---
+++ java/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+java -jar /java-debug-1.0.jar @
--- java/src/main/java/io/comwork/gcp/debug/DemoApplication.java
+++ java/src/main/java/io/comwork/gcp/debug/DemoApplication.java
@@ -2,19 +2,16 @@ package io.comwork.gcp.debug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(DemoApplication.class, args);
-	}
+		Integer WAIT_TIME = Integer.valueOf(System.getenv("WAIT_TIME"));
 
-	@GetMapping("/")
-	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		while(true) {
+			System.out.println("yo");
+			Thread.sleep(WAIT_TIME * 1000);
+		}
 	}
 }
