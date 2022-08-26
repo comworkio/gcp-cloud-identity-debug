1d95ce6f9b8b1aa562b45efe70e4887cdb4e77a6
Idriss Neumann <idriss.neumann@comwork.io>
2022-08-26 14:04:21 +0100

    Writting in GCS

--- java/src/main/java/io/comwork/gcp/debug/DemoApplication.java
+++ java/src/main/java/io/comwork/gcp/debug/DemoApplication.java
@@ -1,7 +1,14 @@
 package io.comwork.gcp.debug;
 
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,7 +17,11 @@ public class DemoApplication {
 		Integer WAIT_TIME = Integer.valueOf(System.getenv("WAIT_TIME"));
 
 		while(true) {
-			System.out.println("yo");
+			String line = String.format("Written at %s", Instant.now());
+			Storage gcs = StorageOptions.newBuilder().setProjectId(System.getenv("GCP_PROJECT")).build().getService();
+			BlobInfo blob = BlobInfo.newBuilder(BlobId.of(System.getenv("GCS_BUCKET_NAME"), System.getenv("BUCKET_FILENAME"))).build();
+			gcs.create(blob, line.getBytes(StandardCharsets.UTF_8));
+			System.out.println(line);
 			Thread.sleep(WAIT_TIME * 1000);
 		}
 	}
